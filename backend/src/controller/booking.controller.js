9e6d8ab7385dc1c8cbf916d346ba3468c1b31909
WebCasting28 <ansarisikkim786@gmail.com>
2026-04-15 12:36:48 +0530

    feat: add email notification on booking creation using Brevo API

--- backend/src/controller/booking.controller.js
+++ backend/src/controller/booking.controller.js
@@ -1,4 +1,10 @@
 import { Booking } from "../model/booking.model.js"
+import SibApiV3Sdk from 'sib-api-v3-sdk';
+
+const client = SibApiV3Sdk.ApiClient.instance;
+client.authentications['api-key'].apiKey = process.env.API_KEY;
+
+const emailsApi = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const creatingBooking = async (req, res) => {
     try {
@@ -21,7 +27,27 @@ const creatingBooking = async (req, res) => {
             mealPlan,
             guests,
             specialRequest
-        })
+        });
+
+        // send email to admin
+
+        await emailsApi.sendTransacEmail({
+            sender: { email: process.env.SENDER_EMAIL, name: "Hotel Booking" },
+            to: [{ email: process.env.ADMIN_EMAIL }],
+            subject: "New Booking Received 🏨",
+
+            htmlContent: ` 
+                <h2>New Booking Details</h2>
+                <p><b>Name:</b> ${name}</p>
+                <p><b>Phone:</b> ${phone}</p>
+                <p><b>Email:</b> ${email}</p>
+                <p><b>Check-In:</b> ${checkInDate}</p>
+                <p><b>Check-Out:</b> ${checkOutDate}</p>
+                <p><b>Room Type:</b> ${roomType || "N/A"}</p>
+                <p><b>Meal Plan:</b> ${mealPlan || "N/A"}</p>
+                <p><b>Guests:</b> ${guests}</p>
+                <p><b>Special Request:</b> ${specialRequest || "None"}</p>`
+        });
 
         return res.status(201).json({
             success: true,
@@ -30,7 +56,7 @@ const creatingBooking = async (req, res) => {
         });
 
     } catch (error) {
-        console.error(error);
+        console.error("Email Error:", error);
 
         return res.status(500).json({
             success: false,
